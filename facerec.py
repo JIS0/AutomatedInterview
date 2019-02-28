e3e163f0aa6953201a891a6c1a4fd7f31ce3eabb
JIS0 <jisojoy@gmail.com>
2019-02-28 16:04:11 +0530

    initial commit

---
+++ facerec.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 12:22:56 2019
+
+@author: JISOMON
+"""
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
